a7d7155525e7011e4f1fdd6018f76e3e515dd9c0
Killigo <Killigo@mail.ru>
2024-02-01 22:34:11 +0300

    fix

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,6 +1,8 @@
 using System.Linq.Expressions;
+using Unity.Burst.CompilerServices;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.XR;
 using static UnityEngine.GraphicsBuffer;
 
 public class Enemy : MonoBehaviour
@@ -9,15 +11,17 @@ public class Enemy : MonoBehaviour
     [SerializeField] private float _speed = 3f;
     [SerializeField] private Collider2D _damageArea;
     [SerializeField] private Collider2D _attackArea;
-    [SerializeField] private Collider2D _lookArea;
     [SerializeField] private Player _player;
-    [SerializeField, Range(0, 360)] private float _viewAngle = 90f;
-    [SerializeField] private float _viewDistance = 15f;
+    [SerializeField, Range(0, 360)] private float _viewAngle = 70f;
+    [SerializeField] private float _viewDistance = 10f;
+    [SerializeField] private float _viewPointOffsetX;
+    [SerializeField] private float _viewPointOffsetY;
 
     private Transform[] _points;
     private Transform _target;
     private int _currentPoint;
     private int directionSide;
+    private Vector3 _eye;
 
     private void Start()
     {
@@ -25,11 +29,13 @@ public class Enemy : MonoBehaviour
 
         for (int i = 0; i < _path.childCount; i++)
             _points[i] = _path.GetChild(i);
+
+        _eye = new Vector3(_viewPointOffsetX, _viewPointOffsetY, 0);
     }
 
     private void Update()
     {
-        if (Vector2.Distance(transform.position, _player.transform.position) < 5)
+        if (IsInView())
         {
             _target = _player.transform;
         }
@@ -38,7 +44,6 @@ public class Enemy : MonoBehaviour
             _target = _points[_currentPoint];
         }
 
-        DrawViewState();
         Move(_target);
 
         if (transform.position == _target.position)
@@ -53,11 +58,45 @@ public class Enemy : MonoBehaviour
         transform.rotation = Quaternion.AngleAxis(directionSide, Vector2.up);
     }
 
+    private bool IsInView()
+    {
+        float realAngle = Vector3.Angle(transform.right + _eye, _player.transform.position - (transform.position + _eye));
+
+        if (name == "Bear")
+        {
+            Debug.Log($"{realAngle} {_viewAngle} {name}");
+        }
+
+        Debug.DrawLine(transform.position + _eye, _player.transform.position, Color.red);
+        DrawViewState();
+
+        if (realAngle < _viewAngle / 2f && Vector3.Distance(transform.position + _eye, _player.transform.position) <= _viewDistance)
+        {
+            Debug.Log("TARGET");
+        }
+        /*
+        RaycastHit hit;
+        
+        if (Physics.Raycast(transform.position + _eye, _player.transform.position - (transform.forward + _eye), out hit, _viewDistance))
+        {
+            Debug.Log("RED");
+
+            if (realAngle < _viewAngle / 2f && Vector3.Distance(transform.position + _eye, _player.transform.position) <= _viewDistance && hit.transform == _player.transform)
+            {
+                Debug.Log("TARGET");
+                return true;
+            }
+        }
+        */
+        return false;
+    }
+
     private void DrawViewState()
     {
-        Vector2 up = transform.position + Quaternion.Euler(new Vector2(0, _viewAngle / 2f)) * (transform.forward * _viewDistance);
-        Vector2 down = transform.position + Quaternion.Euler(-new Vector2(0, _viewAngle / 2f)) * (transform.forward * _viewDistance);
-        Debug.DrawLine(transform.position, up, Color.yellow);
-        Debug.DrawLine(transform.position, down, Color.yellow);
+        Vector3 eye = transform.position + _eye;
+        Vector3 up = eye + Quaternion.Euler(new Vector3(0, 0, _viewAngle / 2f)) * (transform.right * _viewDistance);
+        Vector3 down = eye + Quaternion.Euler(-new Vector3(0, 0, _viewAngle / 2f)) * (transform.right * _viewDistance);
+        Debug.DrawLine(eye, up, Color.yellow);
+        Debug.DrawLine(eye, down, Color.yellow);
     }
 }
